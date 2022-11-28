2ef0314bb7c71e35557c5f1c5a2ed949186230ba
Q506869 <maksim.hermann@bmw.de>
2022-11-28 11:08:39 +0100

    day 1

---
+++ Day1.py
@@ -0,0 +1,16 @@
+import Default.adventofcode as aof
+
+# IMPORT DATA
+# data = aof.getDayData_int(day)
+# data = aof.getDayData_str(day)
+# print("Data:", data)
+
+# PART ONE
+
+
+# aof.answer(ans, day, part)
+
+# PART TWO
+
+
+# aof.answer(ans, day, part)
