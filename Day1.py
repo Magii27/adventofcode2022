e52cbfab131ff032d736d676575fac10dbfe754f
Q506869 <maksim.hermann@bmw.de>
2022-12-01 07:03:22 +0100

    day 1

--- Day1.py
+++ Day1.py
@@ -1,23 +1,38 @@
 import Default.adventofcode as aof
 
 # IMPORT DATA
-splitter = "\n"
+splitter = "\n\n"
 
 # data = aof.getDayData_int(1, splitter)
 data = aof.getDayData_str(1, splitter)
 
 print("Data:", data)
 
-# PART ONE
+# PART ON
 
+high_val = 0
 
-ans = 0
+for elf in data:
+    elf_data = sum(list(map(int, elf.split("\n"))))
+    if elf_data > high_val:
+        high_val = elf_data
+
+ans = high_val
 
 aof.answer(ans, 1, 1)
 
 # PART TWO
 
+high_val = [0, 0, 0]
 
-ans = 0
+for elf in data:
+    elf_data = sum(list(map(int, elf.split("\n"))))
+    for i, high in enumerate(high_val):
+        if elf_data > high:
+            high_val.insert(i, elf_data)
+            high_val.pop()
+            break
+
+ans = sum(high_val)
 
 aof.answer(ans, 1, 2)
